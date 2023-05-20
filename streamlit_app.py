c2a3fe0eca86572e6a1479992a20c8ff3ea88d08
mawu2023 <134164709+mawu2023@users.noreply.github.com>
2023-05-20 21:00:47 +0200

    add fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # load file into df
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# add object picker
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# display the list
 streamlit.dataframe(my_fruit_list)   
