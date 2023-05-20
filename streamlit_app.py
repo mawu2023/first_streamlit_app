963cb164f8bb6fddccbb463318d192d2650f4519
mawu2023 <134164709+mawu2023@users.noreply.github.com>
2023-05-20 21:03:10 +0200

    change fruit list index to Fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # load file into df
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
+
 # add object picker
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # display the list
